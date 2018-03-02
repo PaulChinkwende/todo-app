1ecfbe8a3eeef279aa8cb3da9c9d5c56d41bca04
Paul Chinkwende <paul.chinkwende@bbc.co.uk>
2018-03-02 15:45:15 +0000

    intoduce js to listen for button click

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+
+function buttonClick() {
+  console.log('button clicked');
+}
+
+document.getElementById('add').addEventListener['click', buttonClick];
+
+document.getElementById('add').addEventListener['click', function() {
+  var value = document.getElementById('item').value;
+  if (value){
+    console.log('there was a value')
+
+  }
+
+
+  console.log('hello');
+  console.log(addItem);
+}];
+console.log('hello');
+console.log(add);
+
+function buttonClick() {
+  console.log('button clicked');
+};
